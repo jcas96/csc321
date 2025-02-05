d63e51b24fc08a2bcfd52b76378456f3879d3260
jc <joelcastro.jsc@gmail.com>
2025-02-05 02:10:00 +0000

    menu lab #4 second version

--- jcastro4.java
+++ jcastro4.java
@@ -4,18 +4,17 @@ public class jcastro4
 	public static void main(String[] args)
 	{
 		Scanner input = new Scanner(System.in);
-		System.out.println(“1. Say hello/n2.Say Bye/n3.Say Hola/n4.Say Adios:”);
-		Int choice = input.nextInt();
+		System.out.println("Enter a choice: 1. Say hello, 2.Say Bye, 3.Say Hola, 4.Say Adios");
+		int choice = input.nextInt();
 
 		if(choice ==1){
-			System.out.println(“Hello, Hello, Hello”);
+			System.out.println("Hello, Hello, Hello");
 		}else if(choice ==2){
-			System.out.println(“BYE, Bye, Bye”);
+			System.out.println("BYE, Bye, Bye");
 		}else if(choice ==3){
-			System.out.println(“Hola, Hola, Hola”);
+			System.out.println("Hola, Hola, Hola");
 		}else{
-			System.out.println(“Adios, Adios, Adios”);
+			System.out.println("Adios, Adios, Adios");
 		}
 	}
 }
-
